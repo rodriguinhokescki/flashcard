86cd3c20db2966cfa1cf23e1b0fcac44aa715838
rodriguinhokescki <148064070+rodriguinhokescki@users.noreply.github.com>
2024-10-16 11:04:46 -0300

    upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'História',
+    'Em que ano foi declarada a independencia?',
+    '1822'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a raiz quadrada de 144?',
+    '12'
+)
+
+criaCartao(
+    'Biologia',
+    'Onde fica o fica o fêmur?',
+    'Na coxa'
+)
+
+criaCartao(
+    'Jogos',
+    'Quantos fps o xbox series x roda?',
+    '120fps'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é uma depressão?',
+    'formas de relevo rebaixadas que se formaram a partir de um longo processo de erosão'
+)
+
+criaCartao(
+    'Química',
+    'Qual número atômico da molécula de ouro?',
+    '79'
+)
